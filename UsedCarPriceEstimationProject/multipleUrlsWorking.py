351a9ab830cd5fafa84a8836718bfe0de4b3a342
habibafaisal <habibafaisal8@gmail.com>
2023-03-22 15:11:07 +0500

    Working Code

--- UsedCarPriceEstimationProject/multipleUrlsWorking.py
+++ UsedCarPriceEstimationProject/multipleUrlsWorking.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # define the URLs to scrape
-urls = ["https://www.pakwheels.com/used-cars/search/-/mk_suzuki/md_alto/?page=",
-        "https://www.pakwheels.com/used-cars/search/-/mk_toyota/md_corolla/?page=",
-        "https://www.pakwheels.com/used-cars/search/-/mk_honda/md_civic/?page="]
+urls = ["https://www.pakwheels.com/used-cars/search/-/mk_suzuki/md_alto/md_cultus/md_wagon-r/md_swift/?page=",
+        # "https://www.pakwheels.com/used-cars/search/-/mk_toyota/md_corolla/?page=",
+        # "https://www.pakwheels.com/used-cars/search/-/mk_honda/md_civic/?page="
+        ]
 
 all_data = []
 
 # loop through each URL and page, and scrape the data
 for url in urls:
-    for page in range(1, 4):
+    for page in range(1, 400):
         full_url = url + str(page)
         response = requests.get(full_url)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -28,16 +29,37 @@ for url in urls:
                 price = "N/A"
             pricesList.append(price)
 
+        citiesListing = soup.select('ul.search-vehicle-info')
+        citiesList = []
+
+        for ul in citiesListing:
+            citiesString = ul.get_text(strip=True)
+            print(citiesString)
+            citiesList.append(citiesString)
+
+            # try:
+            #     print('working')
+            #     cities = (li.get_text(strip=True) for li in ul.select("li"))
+            # except ValueError:
+            #     print('not working')
+            #     price = "N/A"
+            # print("============================================================")
+            # print(cities)
+
+
         info = soup.select("ul.search-vehicle-info-2")
         for i, ul in enumerate(info):
             car_name = ul.find_previous("h3").get_text(strip=True)
             car_name = car_name.split("for Sale")[0].strip()
             car_info = [li.get_text(strip=True) for li in ul.select("li")]
-            all_data.append([car_name, *car_info, pricesList[i]])
+            all_data.append([car_name, *car_info, pricesList[i],citiesList[i]])
+            # print(all_data)
 
 # create a pandas dataframe and save the data to a CSV file
 df = pd.DataFrame(
-    all_data, columns=["Car Name", "Year", "KM", "Type", "CC", "Transmission", "Price"]
+    all_data, columns=["Car Name", "Year", "KM", "Type", "CC", "Transmission", "Price", "Cities"]
 )
 df.to_csv("finalData.csv", mode="a", header=True, index=False)
 print('Scraping completed and data saved to finalData.csv.')
+# print(df)
+print(citiesList)
